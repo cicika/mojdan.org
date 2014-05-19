c584fa7f206939a59d3c4ff43134d00816d204b3
Tijana Dojcinovic <tdojcinovic@groundlink.com>
2014-05-19 12:18:17 +0200

    ...

--- src/main/scala/tb_backend/package.scala
+++ src/main/scala/tb_backend/package.scala
@@ -13,11 +13,9 @@ import java.util.Date
 trait Config {
 	val config = ConfigFactory.load()
 	
-	//val dbUrl = "jdbc:postgresql://localhost/ti_biras"
+	
 	val dbUrl = "jdbc:postgresql://localhost/%s" format config.getString("db.database")
 	val dbDriver = "org.postgresql.Driver"
-	//val dbUser = "hackt"
-	//val dbPassword = "b4dp4ss"
 	val dbUser = config.getString("db.username")
 	val dbPassword = config.getString("db.password")
 	val db = Database.forURL(dbUrl, driver = dbDriver, user = dbUser, password = dbPassword)
