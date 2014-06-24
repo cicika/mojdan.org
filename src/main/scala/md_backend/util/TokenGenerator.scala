6be27e7888c1157c85da7c46e00681eb9eee4ac5
Tijana Dojcinovic <tdojcinovic@groundlink.com>
2014-06-24 16:35:59 +0200

    TokenGenerator extended with OTP generation

--- src/main/scala/md_backend/util/TokenGenerator.scala
+++ src/main/scala/md_backend/util/TokenGenerator.scala
@@ -7,12 +7,15 @@ trait TokenGenerator {
   
   val TOKEN_LENGTH = 32
   val REFRESH_TOKEN_LENGTH = 56
+  val OTP_LENGTH = 72
   val TOKEN_CHARS = 
      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
   val secureRandom = new SecureRandom()
     
-  def generateToken:String =  
+  def generateToken: String =  
     generateToken(TOKEN_LENGTH)   
+
+  def generateOtp: String = generateToken(OTP_LENGTH)
   
   def generateToken(tokenLength: Int): String =
     if(tokenLength == 0) "" else TOKEN_CHARS(secureRandom.nextInt(TOKEN_CHARS.length())) + 
