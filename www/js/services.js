7d73ab1f273d89813240535aa1e83adfe1ccf867
Tijana Dojcinovic <tdojcinovic@groundlink.com>
2014-06-26 15:51:59 +0200

    set mdREST service to dynamically obtain location

--- www/js/services.js
+++ www/js/services.js
@@ -4,9 +4,10 @@
 
 var mojDanREST = angular.module('mojDan.services', ['ngResource']);
 
-mojDanREST.factory('mdREST', ['$resource', function($resource){
-    return $resource('user/passreset', {}, {
-      passReset: {method:'POST'},
+mojDanREST.factory('mdREST', ['$resource', '$location', function($resource, $location){
+    return $resource($location.path(), {}, {
+      passReset: {method:'POST',
+    						  url: 'user/passreset'},
       forgotPass: {method: 'POST', 
     							 url: 'user/forgotpass'}
 
