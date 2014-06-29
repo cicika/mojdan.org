2a91086664b6352f42599f5d55f869ed9c2fa314
Tijana Dojcinovic <tdojcinovic@groundlink.com>
2014-06-29 19:32:13 +0200

    ...

--- src/main/scala/md_backend/Server.scala
+++ src/main/scala/md_backend/Server.scala
@@ -3,21 +3,24 @@ package org.mojdan.md_backend
 import akka.actor._
 import akka.io.IO
 import akka.kernel.Bootable
+import akka.routing._
 
 import com.typesafe.config.ConfigFactory
-
-import spray.can.Http
 
 import org.mojdan.md_backend.http._
 import model._
 import app._
 
+import spray.can.Http
+
 object Server extends App with Kernel {
  
 	override def main(args: Array[String]) = {
 		
-		val userActor = system.actorOf(Props[UserActor], "user-actor")
-		val applicationActor = system.actorOf(Props[ApplicationActor], "application-actor")
+		val userActor = system.actorOf(Props[UserActor].withRouter(RoundRobinRouter(nrOfInstances = 5)), "user-actor")
+		val applicationActor = system.actorOf(Props[ApplicationActor].
+																				  withRouter(RoundRobinRouter(nrOfInstances = 15)), "application-actor")
+		val maintenanceActor = system.actorOf(Props[MaintenanceActor], "maintenance-actor")
 
 		val apiService = system.actorOf(Props[TBApiServiceActor], "api-service")
 		val loginService = system.actorOf(Props[LoginServiceActor], "login-service")
@@ -32,6 +35,7 @@ trait Kernel extends Bootable{
 	val systemName = "tibiraskernel"
 	val hostname = config.getString("api.host")
 	lazy implicit val system = ActorSystem(systemName, config)
+
 	def startup = {}
   def shutdown = {} 
 }
